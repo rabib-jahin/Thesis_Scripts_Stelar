aeb9673f3582b90987a6782b5e82f15c334ac079
rabib-jahin <rabibjahin2000@gmail.com>
2023-11-04 22:38:57 +0600

    updated rf score

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,8 +5,10 @@ timeFile="11-taxa-time.csv" # will keep the records of running time
 RFRateFile="11-RFRateFile.csv" # will keep the records of RF rates
 
 
-folders=( 11-taxon  )
-innerFolderNames=(estimated_Xgenes_strongILS/estimated_5genes_strongILS estimated_Xgenes_strongILS/estimated_15genes_strongILS estimated_Xgenes_strongILS/estimated_25genes_strongILS estimated_Xgenes_strongILS/estimated_50genes_strongILS estimated_Xgenes_strongILS/estimated_100genes_strongILS )
+folders=( 37-taxon  )
+innerFolderNames1=(estimated_Xgenes_strongILS/estimated_5genes_strongILS estimated_Xgenes_strongILS/estimated_15genes_strongILS estimated_Xgenes_strongILS/estimated_25genes_strongILS estimated_Xgenes_strongILS/estimated_50genes_strongILS estimated_Xgenes_strongILS/estimated_100genes_strongILS )
+innerFolderNames2=(noscale.25g.500b noscale.50g.500b noscale.100g.500b noscale.200g.250b noscale.200g.500b noscale.200g.1000b noscale.200g.1500b noscale.200g.true noscale.400g.500b noscale.800g.500b scale2d.200g.500b scale2u.200g.500b )
+
 methodNames=( MP MV )
 # Headers of the csv files
 #echo "ilslevel, genetrees, basepair, methodName, modelConditionName,Robinson-Foulds distance" > $RFRateFile
@@ -14,12 +16,25 @@ methodNames=( MP MV )
 
 for folder in ${folders[@]}
 do
+    
 	echo $folder
 	speciesTree=../Dataset/$folder/true_tree_trimmed
 	echo $speciesTree
+	if [ $folder == "11-taxon" ];then
+	innerFolderNames=("${innerFolderNames1[@]}")
 	
-	for inner_folder in ${innerFolderNames[@]}
+	elif [ $folder == "37-taxon" ];then
+	innerFolderNames=("${innerFolderNames2[@]}")
+
+	
+	else
+ echo ""
+	fi
+	
+	echo  ${innerFolderNames[@]}
+	for inner_folder in ${innerFolderNames2[@]}
 	do
+	
 	  
 	 
 		for j in {1..20}
@@ -71,14 +86,17 @@ done
 # Running stelar
 methodNames=( MP MV )
 declare -A sum_diffs
+declare -A sum_rfs
 declare -A count_diffs
 
 
 mkdir -p ../Dataset/$folder/Summary
 timediffcsv=../Dataset/$folder/Summary/average_diffs.csv
+rf_csv=../Dataset/$folder/Summary/average_rfs.csv
 echo "Folder,Method,AverageDiff" > $timediffcsv
 printf "%s,%s,%s,%s\n\n" "folder" "inner_folder" "method" "average_diff" > $timediffcsv
-
+echo "Folder,Method,AverageRF" > $rf_csv
+printf "%s,%s,%s,%s\n\n" "folder" "inner_folder" "method" "average_rf" > $rf_csv
 
 
 for folder in ${folders[@]}
@@ -94,6 +112,7 @@ do
 			# Initialize sum and count for each method
 			sum_diffs[$method]=0
 			count_diffs[$method]=0
+			sum_rfs[$method]=0
 		done
 	  
 	 
@@ -135,8 +154,14 @@ do
 				END=$(date +%s.%N)
 				DIFF=$(echo "$END - $START" | bc)
 				echo $DIFF
+				index_to_extract=2
+				tuple=$(python3 ../RF/getFpFn.py -e $output -t $true_tree)
+				RFdistance=$(echo "$tuple" | cut -d',' -f"$index_to_extract" | tr -d '()')
+				echo $RFdistance
+
 
 				sum_diffs[$method]=$(echo "${sum_diffs[$method]} + $DIFF" | bc)
+				sum_rfs[$method]=$(echo "${sum_rfs[$method]} + $RFdistance" | bc)
                 ((count_diffs[$method]++))
 				
 
@@ -167,6 +192,11 @@ do
                 printf "Average DIFF for %s: %f\n" "$method" "$average_diff"
                 # Uncomment the following line if you want to write the average to a file
                 printf "%s,%s,%s,%.6f\n\n" "$folder" "$inner_folder" "$method" "$average_diff" >> $timediffcsv
+                average_rf=$(echo "${sum_rfs[$method]} / ${count_diffs[$method]}" | bc -l)
+				printf "Average RF for %s: %f\n" "$method" "$average_rf"
+                
+                printf "%s,%s,%s,%.6f\n\n" "$folder" "$inner_folder" "$method" "$average_rf" >> $rf_csv
+              
 
             fi
         done
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,36 @@
+# import re
+# import sys
+
+# def add_branch_length(newick_str):
+
+#     newick_with_lengths = re.sub(r'([^():,;]+)(?=[,);])', r'\1:1', newick_str)
+#     newick_with_lengths = re.sub(r'(\))(?=\))', r'\1:1', newick_with_lengths)
+#     newick_with_lengths = re.sub(r'(\))(?=,)', r'\1:1', newick_with_lengths)
+#     return newick_with_lengths
+
+# if len(sys.argv) != 3:
+#     print("Usage: python script.py input_file output_file")
+#     sys.exit(1)
+
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
+
+# with open(input_file, 'r') as file:
+#     trees = file.readlines()
+
+# output_trees = []
+# for tree in trees:
+#     tree_with_lengths = add_branch_length(tree)
+#     output_trees.append(tree_with_lengths)
+
+# with open(output_file, 'w') as file:
+#     file.writelines(output_trees)
+
 import re
 import sys
 
 def add_branch_length(newick_str):
-
+    # Use a regular expression to add a length of 1 only if there is no existing length.
     newick_with_lengths = re.sub(r'([^():,;]+)(?=[,);])', r'\1:1', newick_str)
     newick_with_lengths = re.sub(r'(\))(?=\))', r'\1:1', newick_with_lengths)
     newick_with_lengths = re.sub(r'(\))(?=,)', r'\1:1', newick_with_lengths)
@@ -20,8 +48,12 @@ with open(input_file, 'r') as file:
 
 output_trees = []
 for tree in trees:
-    tree_with_lengths = add_branch_length(tree)
-    output_trees.append(tree_with_lengths)
+    # Check if there's already a branch length specified and skip modification.
+    if re.search(r'(:\d+\.\d+|:\d+|\d+\.\d+|\d+)', tree):
+        output_trees.append(tree)
+    else:
+        tree_with_lengths = add_branch_length(tree)
+        output_trees.append(tree_with_lengths)
 
 with open(output_file, 'w') as file:
     file.writelines(output_trees)
