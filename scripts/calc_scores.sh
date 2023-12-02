7424876664af60808d5b0c902f1790be2400b2ff
TanjeemAZ <azwadtanjeem@gmail.com>
2023-12-02 07:25:15 +0600

    MP 48 done

--- scripts/calc_scores.sh
+++ scripts/calc_scores.sh
@@ -16,7 +16,7 @@ innerFolderNames48=(0.5X-1000-500 1X-25-500 1X-50-500 1X-100-500 1X-200-500 1X-5
 # outgroups=(11 O GAL STRCA)
 
 # Rooting Methods MAD, MP, MV, OG, RD, NOCHANGE, RAND
-methodNames=(  MP )
+methodNames=(  MP MV MAD OG RAND )
 
 
 # Inputs = Unrooted Gene trees, outputs = rooted gene trees to be used as stelar inputs
@@ -53,119 +53,7 @@ do
 	fi
 	echo  ${innerFolderNames[@]}
 
-	# for inner_folder in ${innerFolderNames[@]}
-	# do
 	
-	# 	for ((j=1;j<=$R;j++))
-	# 	do
-			 
-	# 		gt_folder=$inner_folder/R$j			
-	# 		gt=$gt_folder/all_gt.tre
-			
-	# 		mkdir -p ../Dataset/$folder/$gt_folder/stelar_inputs
-			
-		
-			
-	# 		for method in  ${methodNames[@]}
-	# 		do
-	# 			if [ -f "../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre" ] && [ $fresh -eq 0 ]; then
-	# 				continue
-	# 			fi
-
-	# 			file=../Dataset/$folder/$gt
-	# 			echo -n > ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-				
-	# 			START=$(date +%s.%N)
-	# 			echo here
-	# 			if [ $method == "NOCHANGE" ];then
-	# 				cp $file ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-				
-	# 			elif [ "$method" == "RAND" ]; then
-	# 				# Code for MAD
-	# 				# Add your commands for the MAD method here
-	# 				python3 utils.py $file temp.tre
-	# 				python3 randroot.py $file ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-	# 				echo "RAND"
-
-	# 			elif [ "$method" == "OG" ]; then
-	# 				# Code for OG
-	# 				# Add your commands for the OG method here
-	# 				python3 utils.py $file temp.tre
-	# 				perl reroot_tree.pl -t $file -r $outgroup -o ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-	# 				echo "OG"
-	# 				# python3 ../fastroot/MinVar-Rooting/FastRoot.py -m $method -i temp.tre  -o ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre 
-					
-
-	# 			elif [ "$method" == "MP" ] || [ "$method" == "MV" ]; then
-	# 				# Your existing code for MP and MV
-	# 				python3 utils.py $file temp.tre
-	# 				python3 ../fastroot/MinVar-Rooting/FastRoot.py -m $method -i temp.tre  -o ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre 
-			
-
-	# 			elif [ "$method" == "MAD" ]; then
-	# 				# Code for MAD
-	# 				# Add your commands for the MAD method here
-	# 				python3 utils.py $file temp.tre
-	# 				python3 ../mad/mad.py temp.tre -n
-	# 				cat temp.tre.rooted > ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-					
-	# 				echo "MAD"
-	# 				# python3 ../fastroot/MinVar-Rooting/FastRoot.py -m $method -i temp.tre  -o ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre 
-					
-					
-
-	# 			elif [ "$method" == "RD" ]; then
-					
-	# 				# Code for RD
-	# 				# Add your commands for the RD method here
-	# 				echo "RD"
-	# 				if [ "$inner_folder" == "100gene-true" ] || [ "$inner_folder" == "1000gene-true" ]; then
-	# 					echo "RD and skipping innerfolder $innerfolder for method $method"
-	# 					continue
-	# 				fi
-	# 				echo "in inner folder : $inner_folder"
-	# 				for ((k=1;k<=100;k++))
-	# 				do
-    #                     echo "$k no going"
-    #                     fasta=../Dataset/$folder/$gt_folder/$k/$k.fasta
-    #                     line=$(sed -n "${k}p" "$file")
-    #                     echo $line > temp_gt.tre
-                        
-    #                     rd --msa $fasta --tree temp_gt.tre >> log.txt
-
-    #                     cat temp_gt.tre.rooted.tree >> ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre 
-	# 					echo "" >> ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre
-	# 					rm log.txt
-	# 					rm temp_gt.tre
-	# 					rm temp_gt.tre.rooted.tree
-						
-
-
-	# 			    done
-
-	# 			else
-	# 				echo "Error: Unknown method $method"
-	# 			fi
-
-	# 			END=$(date +%s.%N)
-	# 			DIFF=$(echo "$END - $START" | bc)
-	# 			echo $DIFF
-
-
-	# 			# else
-	# 			# 	python3 utils.py $file temp.tre
-	# 			# 	python3 ../fastroot/MinVar-Rooting/FastRoot.py -m $method -i temp.tre  -o ../Dataset/$folder/$gt_folder/stelar_inputs/stelar_input_$method.tre 
-	# 			# 	END=$(date +%s.%N)
-	# 			# 	DIFF=$(echo "$END - $START" | bc)
-
-	# 			# 	echo $DIFF
-	# 			# fi
-    #          done
-	# 		# break
-	# 	done
-	# 	# break
-	# done
-	# break
 done
 
 
@@ -180,11 +68,8 @@ declare -A count_diffs
 
 # Global csv files for overall Summary
 mkdir -p ../Dataset/Summary
-timediffcsv_main=../Dataset/Summary/average_diffs.csv
 rf_csv_main=../Dataset/Summary/average_rfs.csv
-# echo "Folder,Method,AverageDiff" > $timediffcsv_main
-printf "%s,%s,%s,%s,%s\n\n" "summary_method" "folder" "inner_folder" "rooting_method" "average_diff" > $timediffcsv_main
-# echo "Folder,Method,AverageRF" > $rf_csv_main
+
 printf "%s,%s,%s,%s,%s\n\n" "summary_method" "folder" "inner_folder" "rooting_method" "average_rf" > $rf_csv_main
 
 
@@ -195,11 +80,7 @@ do
 	
 	# Local csv files for each taxa number
 	mkdir -p ../Dataset/$folder/Summary
-	timediffcsv=../Dataset/$folder/Summary/average_diffs.csv
 	rf_csv=../Dataset/$folder/Summary/average_rfs.csv
-	# echo "Folder,Method,AverageDiff" > $timediffcsv
-	printf "%s,%s,%s,%s,%s\n\n" "summary_method" "folder" "inner_folder" "rooting_method" "average_diff" > $timediffcsv
-	# echo "Folder,Method,AverageRF" > $rf_csv
 	printf "%s,%s,%s,%s,%s\n\n" "summary_method" "folder" "inner_folder" "rooting_method" "average_rf" > $rf_csv
 
 
@@ -234,14 +115,8 @@ do
 	 
 		for ((j=1;j<=$R;j++))
 		do
-		#  if [[ $temp_ -eq 1 ]] ;then
-
-        #     break
-
-		#  fi
 
 			 
-
 			mkdir -p ../Dataset/$folder/$inner_folder/R$j/stelar_outputs/
 			for method in  ${methodNames[@]}
 			do
@@ -269,19 +144,7 @@ do
 				output=../Dataset/$folder/$inner_folder/R$j/stelar_outputs/stelar_output_$method.tre
 				stripped_output=../Dataset/$folder/$inner_folder/R$j/stelar_outputs/stelar_output_$method.stripped.tre
 				true_tree=../Dataset/$folder/true_tree_trimmed
-				# touch $output
-				# echo -n > $output
-				# echo -n > $input_sanitized
-				
 
-
-				# python3 helper.py $input $input_sanitized
-
-				# START=$(date +%s.%N)			 
-				# java -jar stelar.jar -i $input_sanitized -o $output	
-				# END=$(date +%s.%N)
-				# DIFF=$(echo "$END - $START" | bc)
-				# echo $DIFF
 
 				index_to_extract=2
 				tuple=$(python3 ../RF/getFpFn.py -e $output -t $true_tree)
@@ -297,9 +160,7 @@ do
 
             done
 			
-			# rm -r ../Dataset/$folder/$inner_folder/R$j/stelar_inputs
-			# rm -r ../Dataset/$folder/$inner_folder/R$j/stelar_outputs
-			# break
+
 
 		done
 
@@ -310,11 +171,7 @@ do
 		for method in  ${methodNames[@]}
         do
             if [ ${count_diffs[$method]} -gt 0 ]; then
-                # average_diff=$(echo "${sum_diffs[$method]} / ${count_diffs[$method]}" | bc -l)
-                # printf "Average DIFF for %s: %f\n" "$method" "$average_diff"
-                # # Uncomment the following line if you want to write the average to a file
-                # printf "stelar,%s,%s,%s,%.6f\n" "$folder" "$inner_folder" "$method" "$average_diff" >> $timediffcsv
-				# printf "stelar,%s,%s,%s,%.6f\n" "$folder" "$inner_folder" "$method" "$average_diff" >> $timediffcsv_main
+
                 average_rf=$(echo "${sum_rfs[$method]} / ${count_diffs[$method]}" | bc -l)
 				printf "Average RF for %s: %f\n" "$method" "$average_rf"
                 
@@ -324,9 +181,6 @@ do
 
             fi
         done
-
-		# printf "\n" >> $timediffcsv
-		# printf "\n" >> $timediffcsv_main
 		
 		printf "\n" >> $rf_csv
 		printf "\n" >> $rf_csv_main
